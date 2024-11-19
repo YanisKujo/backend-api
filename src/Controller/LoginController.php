678c0448a85c28bbf484d44f007e4636a0a7429a
Ruben alvedin <ruben.alvedin@etudiant.univ-reims.fr>
2024-11-19 14:38:06 +0100

    feat: generateTokenForUser in LoginController

--- src/Controller/LoginController.php
+++ src/Controller/LoginController.php
@@ -7,9 +7,15 @@ use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Attribute\Route;
 use Symfony\Component\Security\Http\Attribute\CurrentUser;
+use App\Service\Tokens;
 
 class LoginController extends AbstractController
 {
+    public function __construct(
+        private Tokens $tokens,
+    ) {
+    }
+
     #[Route('/api/login', name: 'api_login', methods: ['POST'])]
     public function index(#[CurrentUser] ?User $user): Response
     {
@@ -17,6 +23,8 @@ class LoginController extends AbstractController
             throw $this->createAccessDeniedException();
         }
 
-        return $this->json(['token' => 'votre-super-token', 'user' => $user->getUserIdentifier()]);
+        $token = $this->tokens->generateTokenForUser($user->email);
+
+        return $this->json(['token' => $token, 'user' => $user->getUserIdentifier()]);
     }
 }
